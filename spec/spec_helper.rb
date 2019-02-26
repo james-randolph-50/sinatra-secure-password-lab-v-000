e1684b1a0929a72817db6166957958a79c09b991
James Randolph <james.randolph2016@gmail.com>
2019-02-26 17:50:49 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,27 +8,6 @@ require 'capybara/dsl'
 require 'rack_session_access/capybara'
 
 
-if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
-end
-
-ApplicationController.configure do |app|
-  app.use RackSessionAccess::Middleware
-end
-
-RSpec.configure do |config|
-
-  config.include Rack::Test::Methods
-  config.include Capybara::DSL
-
-
-  config.order = 'default'
-end
-
-def app
-  Rack::Builder.parse_file('config.ru').first
-end
-
 Capybara.app = app
 
 # def session
